fc357c8b1f1e1f69bf82b42b356a91f86aba010a
André Prolo <proloandre@hotmail.com>
2023-10-22 12:02:46 -0300

    saving some work

--- c4_docs/workspace.dsl
+++ c4_docs/workspace.dsl
@@ -3,7 +3,9 @@ workspace {
   model {
     user = person "Woodworking Professional"
       timberWatchSystem = softwareSystem "Timber Watch" {
-        timberWatchApi = container "Timber Watch Api" "Elixir/Phoenix" "Elixir/Phoenix"
+        timberWatchApi = container "Timber Watch Api" "Elixir/Phoenix" "Elixir/Phoenix" {
+          signinController = component "Sign In Controller" "Allows users to sign in to the Internet Banking System." "Spring MVC Rest Controller"
+        }
         singlePageApplication = container "Web Application" "Provides a real-time view of all the metrics using Phoenix Live View" "HTML/JavaScript" "Web Browser"
         timberWatchDb = container "Database" "Stores sensors metrics in time-series tables" "TimescaleDB/PostgreSQL" "Database"
       }
@@ -15,6 +17,8 @@ workspace {
       timberWatchApi -> singlePageApplication "Delivers to the user web browser"
       timberWatchApi -> timberWatchDb "Reads from and writes to" "TCP/SQL"
       sensorSystem -> timberWatchApi "Publishes real time data to" "WebSocket"
+
+      singlePageApplication -> signinController
   }
 
   views {
--- scripts/esp32-marotagem-rust/src/main.rs
+++ scripts/esp32-marotagem-rust/src/main.rs
@@ -1,4 +1,10 @@
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
+use embedded_svc::wifi::ClientConfiguration;
+use embedded_svc::wifi::Configuration;
+use esp_idf_svc::wifi::EspWifi;
+use esp_idf_hal::prelude::Peripherals;
+use esp_idf_svc::nvs::EspDefaultNvsPartition;
+use esp_idf_svc::eventloop::EspSystemEventLoop;
 use log::*;
 
 use std::time::Duration;
@@ -15,8 +21,36 @@ fn main() {
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    network::wifi::connect_to_wifi("Aluno Unisep", "alunounisep");
+    // network::wifi::connect_to_wifi("Aluno Unisep", "alunounisep");
     // network::wifi::connect_to_wifi("Segfy", "12fyseg3");
+    let ssid = "Aluno Unisep";
+    let password = "alunounisep";
+
+    let peripherals = Peripherals::take().unwrap();
+    let sys_loop = EspSystemEventLoop::take().unwrap();
+    let nvs = EspDefaultNvsPartition::take().unwrap();
+
+    let mut wifi_driver = EspWifi::new(
+        peripherals.modem,
+        sys_loop,
+        Some(nvs)
+    ).unwrap();
+
+    wifi_driver.set_configuration(&Configuration::Client(ClientConfiguration{
+        ssid: ssid.into(),
+        password: password.into(),
+        ..Default::default()
+    })).unwrap();
+
+    wifi_driver.start().unwrap();
+    wifi_driver.connect().unwrap();
+
+    while !wifi_driver.is_connected().unwrap(){
+        let config = wifi_driver.get_configuration().unwrap();
+        println!("Waiting for station {:?}", config);
+    }
+    info!("Should be connected now");
+    println!("IP info: {:?}", wifi_driver.sta_netif().get_ip_info().unwrap());
 
     sleep(Duration::new(5, 0));
     let mut client = network::socket_client::connect_to_websocket().unwrap();
